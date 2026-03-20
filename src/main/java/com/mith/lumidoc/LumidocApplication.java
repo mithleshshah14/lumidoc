f6236b08bf1ff9a97d06fe6ccd2b0b0cfc66e21a
Mithlesh Shah <mithleshshah84@gmail.com>
2026-03-20 14:30:44 +0530

    Initial commit

---
+++ src/main/java/com/mith/lumidoc/LumidocApplication.java
@@ -0,0 +1,13 @@
+package com.mith.lumidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumidocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumidocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mith/lumidoc/LumidocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mith.lumidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumidocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
